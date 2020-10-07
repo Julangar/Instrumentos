07a4c17593666e47972db4891fcfc00b7597c0b6
Julangar <jagarciaguer@msn.com>
2020-10-07 18:34:39 -0500

    Terminado

--- instrumento.py
+++ instrumento.py
@@ -1,6 +1,8 @@
 import random
 import abc
 from abc import ABC, abstractmethod
+
+musicos = []
 class Instrumento(ABC):
 
     @abstractmethod
@@ -20,22 +22,73 @@ class Instrumento(ABC):
 
 class Guitarra(Instrumento):
 
-    def __init__(self):
-        pass
-
     def afinar(self):
         return print("Afinando guitarra")
 
     def tocar(self):
         return print("Tocando guitarra")
 
-    def tocar_nota(self, nota):
-        return print("Tocando guitarra en ",nota)
+    def tocar_nota(self):
+
+        opc2 = random.randrange(7)
+        if(opc2 == 1):
+            nota = "Do"
+            return print("Tocando guitarra en ",nota)
+        elif(opc2 == 2):
+            nota = "Re"
+            return print("Tocando guitarra en ",nota)
+        elif(opc2 == 3):
+            nota = "Mi"
+            return print("Tocando guitarra en ",nota)
+        elif(opc2 == 4):
+            nota = "Fa"
+            return print("Tocando guitarra en ",nota)
+        elif(opc2 == 5):
+            nota = "Sol"
+            return print("Tocando guitarra en ",nota)
+        elif(opc2 == 6):
+            nota = "La"
+            return print("Tocando guitarra en ",nota)
+        elif(opc2 == 7):
+            nota = "Si"
+            return print("Tocando guitarra en ",nota)
+
+
+class Violin(Instrumento):
+
+    def afinar(self):
+        return print("Afinando violin")
+
+    def tocar(self):
+        return print("Tocando violin")
+
+    def tocar_nota(self):
+
+        opc2 = random.randrange(7)
+        if(opc2 == 1):
+            nota = "Do"
+            return print("Tocando violin en ",nota)
+        elif(opc2 == 2):
+            nota = "Re"
+            return print("Tocando violin en ",nota)
+        elif(opc2 == 3):
+            nota = "Mi"
+            return print("Tocando violin en ",nota)
+        elif(opc2 == 4):
+            nota = "Fa"
+            return print("Tocando violin en ",nota)
+        elif(opc2 == 5):
+            nota = "Sol"
+            return print("Tocando violin en ",nota)
+        elif(opc2 == 6):
+            nota = "La"
+            return print("Tocando violin en ",nota)
+        elif(opc2 == 7):
+            nota = "Si"
+            return print("Tocando violin en ",nota)
+
 
 class Bajo(Instrumento):
-
-    def __init__(self):
-        pass
 
     def afinar(self):
         return print("Afinando bajo")
@@ -43,90 +96,75 @@ class Bajo(Instrumento):
     def tocar(self):
         return print("Tocando bajo")
 
-    def tocar_nota(self, nota):
-        return print("Tocando bajo en ",nota)
+    def tocar_nota(self):
 
-class Violin (Instrumento):
-
-    def __init__(self):
-        pass
-
-    def afinar(self):
-        return print("Afinando violin...")
-
-    def tocar(self):
-        return print("Tocando violin...")
-
-    def tocar_nota(self,nota):
-        return print("Tocando violin en ",nota,"...")
+        opc2 = random.randrange(7)
+        if(opc2 == 1):
+            nota = "Do"
+            return print("Tocando bajo en ",nota)
+        elif(opc2 == 2):
+            nota = "Re"
+            return print("Tocando bajo en ",nota)
+        elif(opc2 == 3):
+            nota = "Mi"
+            return print("Tocando bajo en ",nota)
+        elif(opc2 == 4):
+            nota = "Fa"
+            return print("Tocando bajo en ",nota)
+        elif(opc2 == 5):
+            nota = "Sol"
+            return print("Tocando bajo en ",nota)
+        elif(opc2 == 6):
+            nota = "La"
+            return print("Tocando bajo en ",nota)
+        elif(opc2 == 7):
+            nota = "Si"
+            return print("Tocando bajo en ",nota)
 
 class Persona(object):
 
     def __init__(self,nombre):
         self.nombre = nombre
 
-    def presentar(self):
-        print("Hola mi nombre es ",self.nombre)
+    def presentar(self, nombre):
+        print("Hola mi nombre es ",musicos[nombre])
 
 
 class Musico (Persona):
 
-    def tocar(self, i = Instrumento ):
-        i.afinar
-        i.tocar
-        i.tocar_nota
+    def tocar(self, j):
+        j.afinar()
+        j.tocar()
+        j.tocar_nota()
 
-class Banda:
+class Banda(object):
 
-    def __init__(self):
-        self.musicos = []
-
-    def agregar_musico(self, nombre, m = Musico ):
-        self.musicos.append(m.nombre)
-        return print(self.musicos)
+    def agregar_musico(self, nombre):
+        musicos.append(nombre)
+        return musicos
 
     def generar_instrumento(self):
-        i = Instrumento
         opc = random.randrange(3)
         if(opc == 1):
-            i = Guitarra
-        if(opc == 2):
-            i = Bajo
-        if(opc == 3):
-            i = Violin
+            l = Guitarra()
+            return l
+        elif(opc == 2):
+            i = Bajo()
+            return i
+        elif(opc == 3):
+            j = Violin()
+            return j
+
     
     def presentar_banda(self):
         m = Musico
-        musicos = []
-        for m in musicos:
-            m.presentar()
-            m.tocar(generar_instrumento())
-
-#Hola         
+        tamaño_lista = len(musicos)
+        for i in range(0, tamaño_lista):
+            m.presentar("", i)
+            m.tocar("",b.generar_instrumento(""))
+     
 b = Banda
 b.agregar_musico("","Pepito")
 b.agregar_musico("","Juan")
 b.agregar_musico("","Mariana")
-
-
-"""
-opc2 = random.randrange(7)
-
-
-i.afinar("")
-i.tocar("")
-if(opc2 == 1):
-    i.tocar_nota("","Do")
-if(opc2 == 2):
-    i.tocar_nota("","Re")
-if(opc2 == 3):
-    i.tocar_nota("","Mi")
-if(opc2 == 4):
-    i.tocar_nota("","Fa")
-if(opc2 == 5):
-    i.tocar_nota("","Sol")
-if(opc2 == 6):
-    i.tocar_nota("","La")
-if(opc2 == 7):
-    i.tocar_nota("","Si")
-"""
+b.presentar_banda("")
